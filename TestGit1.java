e0dab615988851e2139f7a85dac16c6b501c2324
Bernardino Di Biagio <b.dibiagio@goreply.io>
2025-02-27 17:46:41 +0100

    [MOD] modifica TestGit!.java branch sviluppo

--- TestGit1.java
+++ TestGit1.java
@@ -1,5 +1,5 @@
 public class TestGit1 {
-    static String modifica = "branch SVILUPPO"
+    static String modifica = "branch SVILUPPO dopo modifica"
 
 	public static void main(String[] args) {
         System.out.println(modifica)
